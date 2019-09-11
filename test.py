e050a2a1cf13e940b9533bbdf966f5d1c9c80f18
mydeskmate <516622145@qq.com>
2019-09-11 19:05:40 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+r = requests.get('https://api.github.com/requests/kennethreitz/requests/issues/482')
+
+print(r.url)
+
+
+r = requests.get(r.url + u'/comments')
+print(r.status_code)
+# issue = json.loads(r.text)
+# print(issue)
+# print(issue[u'documentation_url'])
